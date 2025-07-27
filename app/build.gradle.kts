7f6ec6e5c883c63dcf5d9e75fdfaf6ffb8a99f98
Karolina Droscova <drosckar@fit.cvut.cz>
2025-07-27 22:04:51 +0200

    app dependencies on modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,4 +85,21 @@ dependencies {
 
     // Timber
     implementation(libs.timber)
+
+    // APP MODULES
+    implementation(projects.core.presentation.designsystem)
+    implementation(projects.core.presentation.ui)
+    implementation(projects.core.domain)
+    implementation(projects.core.data)
+    implementation(projects.core.database)
+
+    implementation(projects.auth.presentation)
+    implementation(projects.auth.domain)
+    implementation(projects.auth.data)
+
+    implementation(projects.run.presentation)
+    implementation(projects.run.domain)
+    implementation(projects.run.data)
+    implementation(projects.run.location)
+    implementation(projects.run.network)
 }
