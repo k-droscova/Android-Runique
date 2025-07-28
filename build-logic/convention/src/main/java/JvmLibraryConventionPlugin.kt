a37da9f3b31418500b54b19f7488544fa1358a18
Karolina Droscova <drosckar@fit.cvut.cz>
2025-07-28 21:17:03 +0200

    jvm convention plugin

--- build-logic/convention/src/main/java/JvmLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/JvmLibraryConventionPlugin.kt
@@ -1,2 +1,14 @@
-class JvmLibraryConventionPlugin {
+import com.example.drosckar.convention.configureKotlinJvm
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class JvmLibraryConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.apply("org.jetbrains.kotlin.jvm")
+
+            configureKotlinJvm()
+        }
+    }
 }
