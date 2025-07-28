a3de3978e5aad5b7780a4ce286d3168841cae6eb
Karolina Droscova <drosckar@fit.cvut.cz>
2025-07-28 21:13:04 +0200

    optimized imports

--- build-logic/convention/src/main/java/AndroidFeatureUiConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidFeatureUiConventionPlugin.kt
@@ -1,15 +1,7 @@
-import com.android.build.api.dsl.LibraryExtension
-import com.example.drosckar.convention.ExtensionType
 import com.example.drosckar.convention.addUiLayerDependencies
-import com.example.drosckar.convention.configureAndroidCompose
-import com.example.drosckar.convention.configureBuildTypes
-import com.example.drosckar.convention.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.kotlin
 
 class AndroidFeatureUiConventionPlugin: Plugin<Project> {
     override fun apply(target: Project) {
--- build-logic/convention/src/main/java/AndroidLibraryComposeConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryComposeConventionPlugin.kt
@@ -1,14 +1,8 @@
 import com.android.build.api.dsl.LibraryExtension
-import com.example.drosckar.convention.ExtensionType
 import com.example.drosckar.convention.configureAndroidCompose
-import com.example.drosckar.convention.configureBuildTypes
-import com.example.drosckar.convention.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.kotlin
 
 class AndroidLibraryComposeConventionPlugin: Plugin<Project> {
     override fun apply(target: Project) {
