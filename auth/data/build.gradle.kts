592744aceb977bc816ddcde278a73554335f4f7a
Karolina Droscova <drosckar@fit.cvut.cz>
2025-07-28 21:25:07 +0200

    ktor convention plugin

--- auth/data/build.gradle.kts
+++ auth/data/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.runique.android.library)
+    alias(libs.plugins.runique.jvm.ktor)
 }
 
 android {
@@ -7,14 +8,6 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
     implementation(projects.auth.domain)
     implementation(projects.core.domain)
     implementation(projects.core.data)
