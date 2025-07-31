22f8020953f22aae1d5976b1f6262ae7e75c5186
Karolina Droscova <drosckar@fit.cvut.cz>
2025-07-31 16:20:16 +0200

    session storage mechanism, NOTE: look up different ways for storage since EncryptedSharedPreferences is deprectaed

---
+++ core/data/src/main/java/com/example/drosckar/core/data/auth/AuthInfoSerializable.kt
@@ -0,0 +1,9 @@
+package com.example.drosckar.core.data.auth
+
+import kotlinx.serialization.Serializable
+@Serializable
+data class AuthInfoSerializable(
+    val accessToken: String,
+    val refreshToken: String,
+    val userId: String
+)
--- core/data/src/main/java/com/example/drosckar/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/example/drosckar/core/data/di/CoreDataModule.kt
@@ -1,10 +1,15 @@
 package com.example.drosckar.core.data.di
 
+import com.example.drosckar.core.data.auth.EncryptedSessionStorage
 import com.example.drosckar.core.data.networking.HttpClientFactory
+import com.example.drosckar.core.domain.util.SessionStorage
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
         HttpClientFactory().build()
     }
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
